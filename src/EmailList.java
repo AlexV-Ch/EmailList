8fea908afcd69613a3794733eda242919bf8ab0b
auto_1 <alexch@yandex.ru>
2025-02-17 15:19:08 +0300

    11.6 Практическая работа. Множества — TreeSet v9

--- src/EmailList.java
+++ src/EmailList.java
@@ -3,20 +3,20 @@ import java.util.TreeSet;
 public class EmailList {
     TreeSet<String> email = new TreeSet<>();
 
-public void listEmail(String textEmail) {
-    String[] words =textEmail.split("\\s+");
-    String kom =words[0];
-    //String regex = "[0-9a-zA-Z]+" + "[@]" + "[a-zA-Z]+" +"\\."+ "[a-zA-Z]{2,3}";
+    public void listEmail(String textEmail) {
+        String[] words = textEmail.split("\\s+");
+        String kom = words[0];
+        //String regex = "[0-9a-zA-Z]+" + "[@]" + "[a-zA-Z]+" +"\\."+ "[a-zA-Z]{2,3}";
 
-    if (words.length <= 1 && !kom.equals("LIST")) {
-        System.out.println("Вы не ввели адрес электронной почты");
-        return;
-    }
-    switch (kom) {
-        case "ADD" -> emailAdd(words[1]);
-        case "LIST" -> printList(email);
-        default -> System.out.println("Нет такой команды");
-    }
+        if (words.length <= 1 && !kom.equals("LIST")) {
+            System.out.println("Вы не ввели адрес электронной почты");
+            return;
+        }
+        switch (kom) {
+            case "ADD" -> emailAdd(words[1]);
+            case "LIST" -> printList(email);
+            default -> System.out.println("Нет такой команды");
+        }
 
 //    if (!kom.equals("ADD") && !kom.equals("LIST")) {
 //        System.out.println("Нет такой команды");
@@ -37,25 +37,27 @@ public void listEmail(String textEmail) {
 //        }
 //    }
 
-}
-private void printList(TreeSet<String> email) {
-    if (!email.isEmpty()) {
-        System.out.println("Список адресов электронной почты");
-    for (String eml : email) {
-        System.out.println(eml);
     }
-    }else {
-        System.out.println("Список адресов пуст");
+
+    private void printList(TreeSet<String> email) {
+        if (!email.isEmpty()) {
+            System.out.println("Список адресов электронной почты");
+            for (String eml : email) {
+                System.out.println(eml);
+            }
+        } else {
+            System.out.println("Список адресов пуст");
+        }
     }
-}
+
     private boolean isValidEmail(String email) {
         // Регулярное выражение для проверки email
         String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
         return email.matches(regex);
     }
 
-    private void emailAdd (String words1 ) {
-         if (isValidEmail(words1)) {
+    private void emailAdd(String words1) {
+        if (isValidEmail(words1)) {
             email.add(words1);
             System.out.println("Добавлен адрес " + words1);
         } else {
